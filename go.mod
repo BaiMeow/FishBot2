4ebb2db43477f4a3eb6a043ddfd5a2bbbcc341d5
MscBaiMeow <baishu2003@outlook.com>
2021-01-09 20:56:57 +0800

    login in server

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/MscBaiMeow/FishbotReload
+
+go 1.14
+
+require (
+	github.com/Tnze/go-mc v1.16.2-0.20210109052551-0e959dfbc000
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4
+	github.com/mattn/go-colorable v0.0.9
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/spf13/viper v1.7.1
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Tnze/go-mc/bot"
+	"github.com/Tnze/go-mc/yggdrasil"
+	"github.com/go-toast/toast"
+	"github.com/mattn/go-colorable"
+	"github.com/spf13/viper"
+)
+
+var (
+	c     *bot.Client
+	watch chan time.Time
+)
+
+func main() {
+	log.SetOutput(colorable.NewColorableStdout())
+	vp := viper.New()
+	vp.SetConfigName("config")
+	vp.SetConfigType("toml")
+	vp.AddConfigPath(".")
+	vp.SetDefault("profile", map[string]string{"account": "example@example.com", "passwd": "123456789", "name": "Steve"})
+	vp.SetDefault("setting", map[string]interface{}{
+		"timeout": 45,
+		"ip":      "mc.hypixel.net",
+		"port":    25565,
+	})
+	if err := vp.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			vp.SafeWriteConfig()
+			if err := sendNotification("配置文件缺失，已创建默认配置文件，请打开\"config.toml\"修改并保存", 1); err != nil {
+				log.Println(err)
+			}
+			log.Fatal("配置文件缺失，已创建默认配置文件，请打开\"config.toml\"修改并保存")
+		} else {
+			log.Fatal(err)
+		}
+	}
+	c = bot.NewClient()
+	resp, err := yggdrasil.Authenticate(vp.GetString("profile.account"), vp.GetString("profile.passwd"))
+	if err != nil {
+		log.Fatal("Authenticate:", err)
+	}
+	c.Auth.UUID, c.Auth.Name = resp.SelectedProfile()
+	c.Auth.AsTk = resp.AccessToken()
+	vp.Debug()
+	vp.Set("profile.name", c.Auth.Name)
+	vp.Set("profile.uuid", c.Auth.UUID)
+	vp.Set("profile.astk", c.Auth.AsTk)
+	vp.Set("profile.account", vp.GetString("profile.account"))
+	vp.Set("profile.passwd", vp.GetString("profile.passwd"))
+	vp.Debug()
+	vp.WriteConfig()
+
+	c.Events.GameStart = onGameStart
+
+	for {
+		if err := c.JoinServer(vp.GetString("setting.ip"), vp.GetInt("setting.port")); err != nil {
+			log.Fatal(err)
+		}
+
+		if err = c.HandleGame(); err != nil {
+			log.Fatal(err)
+		}
+		sendNotification("失去与服务器的连接，将在五秒后重连", 0)
+		log.Println("Reconnect to server in 5s")
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func onGameStart() error {
+	sendNotification("成功进入游戏", 0)
+	log.Println("Game starts.")
+	return nil
+}
+
+func sendNotification(content string, level int8) error {
+	notification := toast.Notification{
+		AppID:   "MscFishBot",
+		Message: content,
+	}
+	switch level {
+	case 0:
+		notification.Title = "Msc钓鱼机器人 普通消息"
+	case 1:
+		notification.Title = "Msc钓鱼机器人 警告"
+	default:
+		notification.Title = "Msc钓鱼机器人"
+	}
+	return notification.Push()
+}
